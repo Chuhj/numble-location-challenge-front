dffd8e6fe604873e73ee2f4d61f78a825284063c
Chuhj <chj9574@naver.com>
2022-11-26 00:50:56 +0900

    feat: ✨ 로그인, 회원가입, 로그인 확인 함수 구현

---
+++ src/api/auth.ts
@@ -0,0 +1,35 @@
+import { useMutation } from 'react-query';
+import { AxiosError, AxiosResponse } from 'axios';
+import axiosInstance from '../api/config/axios';
+import { LoginInputs } from './../views/Login/index';
+import { SignupInputs } from '../views/Signup';
+import { Response, ErrorResponse } from './types';
+
+interface SignupBody extends SignupInputs {
+  userType: string;
+}
+
+export const getRefresh = async () => {
+  const res = await axiosInstance.post(`/refresh`);
+  return setHeadersToken(res);
+};
+
+export const setHeadersToken = (data: AxiosResponse) => {
+  const access_token = data.headers.authorization;
+  if (!access_token) throw new Error('토큰 없음');
+  axiosInstance.defaults.headers.common['authorization'] = `Bearer ${access_token}`;
+};
+
+export const useSignup = () => {
+  return useMutation<AxiosResponse<Response>, AxiosError<ErrorResponse>, SignupBody>(async (inputs) => {
+    const res = await axiosInstance.post(`/join`, inputs);
+    return res;
+  });
+};
+
+export const useLogin = () => {
+  return useMutation<AxiosResponse<Response>, AxiosError<ErrorResponse>, LoginInputs>(async (inputs) => {
+    const res = await axiosInstance.post(`/login`, inputs);
+    return res;
+  });
+};
