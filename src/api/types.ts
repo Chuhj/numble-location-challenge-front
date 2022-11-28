d0ed87ff0e544a3d86cd2a2f9f453ca82da0fe69
Chuhj <chj9574@naver.com>
2022-11-28 19:19:10 +0900

    feat: 🏷️ Social 타입 추가

--- src/api/types.ts
+++ src/api/types.ts
@@ -1,7 +1,33 @@
+export interface Social {
+  socialings: any[];
+  id: number;
+  images: string[];
+  regionCode: number;
+  dongCode: number;
+  dongName: string;
+  title: string;
+  endDate: string;
+  currentNums: number;
+  limitedNums: number;
+  status: 'AVAILABLE' | 'EXPIRATION';
+  category: {
+    id: number;
+    name: '문화활동' | '운동/액티비티' | '음식' | '취미/창작' | '여행';
+  };
+  tags: [
+    {
+      tag_id: number;
+      tag_name: string;
+    }
+  ];
+  likeCnt: number;
+  createDate: string;
+}
+
 export interface Response {
   success: boolean;
   message: string;
-  data: any[];
+  data: any;
 }
 
 export interface ErrorResponse {
