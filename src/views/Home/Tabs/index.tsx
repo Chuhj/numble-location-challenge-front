c5d6873313d66a5949a6925155584cea0cb05957
Chuhj <chj9574@naver.com>
2022-11-30 14:40:43 +0900

    feat: ✨ 카테고리 탭 이동 구현

--- src/views/Home/Tabs/index.tsx
+++ src/views/Home/Tabs/index.tsx
@@ -1,27 +1,23 @@
 import { TabsWrapper, TabItem, TabName } from './styles';
 
-export default function Tabs() {
+export default function Tabs({ tab, onClickTabs }: { tab: number; onClickTabs: React.MouseEventHandler }) {
   return (
     <nav>
-      <TabsWrapper>
-        <TabItem>
-          <TabName selected>전체</TabName>
-        </TabItem>
-        <TabItem>
-          <TabName>문화활동</TabName>
-        </TabItem>
-        <TabItem>
-          <TabName>운동/액티비티</TabName>
-        </TabItem>
-        <TabItem>
-          <TabName>음식</TabName>
-        </TabItem>
-        <TabItem>
-          <TabName>취미/창작</TabName>
-        </TabItem>
-        <TabItem>
-          <TabName>여행</TabName>
-        </TabItem>
+      <TabsWrapper onClick={onClickTabs}>
+        {[
+          ['전체', 0],
+          ['문화활동', 1],
+          ['운동/액티비티', 2],
+          ['음식', 3],
+          ['취미/창작', 4],
+          ['여행', 5],
+        ].map((category) => (
+          <TabItem key={category[0]}>
+            <TabName selected={tab === category[1]} id={String(category[1])}>
+              {category[0]}
+            </TabName>
+          </TabItem>
+        ))}
       </TabsWrapper>
     </nav>
   );
