65c5ed2f78b6754b9ada9c3a79a3d207af1e7fa5
Chuhj <chj9574@naver.com>
2022-12-01 19:18:40 +0900

    feat: ✨ 모임 검색 api 추가

--- src/api/social.ts
+++ src/api/social.ts
@@ -1,6 +1,7 @@
-import { useQuery } from 'react-query';
+import { AxiosError } from 'axios';
+import { useMutation, useQuery } from 'react-query';
 import axiosInstance from './config/axios';
-import { Social } from './types';
+import { ErrorResponse, Social } from './types';
 
 export const useGetSocialList = (sort: string, category: number) => {
   let sortNum: number;
@@ -9,6 +10,13 @@ export const useGetSocialList = (sort: string, category: number) => {
   if (sort === 'popular') sortNum = 3;
   return useQuery<Social[]>(['socials', sort, category], async () => {
     const res = await axiosInstance.get(`/social${category === 0 ? '' : `/${category}`}/sort/${sortNum}`);
+    return res.data.data;
+  });
+};
+
+export const useSearchSocials = () => {
+  return useMutation<Response, AxiosError<ErrorResponse>, { tagId: string }>(['socials', 'search'], async ({ tagId }) => {
+    const res = await axiosInstance.get(`/social/search/${tagId}`);
     return res.data.data;
   });
 };
