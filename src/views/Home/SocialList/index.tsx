251c189e2d0960c039f88f96b18e04f98da6989d
Chuhj <chj9574@naver.com>
2022-12-14 03:38:50 +0900

    refactor: ♻️ 데이터를 props로 전달X 컴포넌트 안에서 데이터를 받도록 변경

--- src/views/Home/SocialList/index.tsx
+++ src/views/Home/SocialList/index.tsx
@@ -2,24 +2,27 @@ import styled from 'styled-components';
 import { fontStyle } from '../../../common/styles/FontStyle';
 import more from '../../../common/styles/assets/more.svg';
 import SocialCardList from '../SocialCardList';
-import { Social } from '../../../api/types';
+import { useGetSocialList } from '../../../api/social';
 
 interface SocialListProps {
   title: string;
-  socials?: Social[];
+  tab: number;
+  kind: string;
   marginTop?: number;
   gap?: number;
   onClickHeader?: React.MouseEventHandler;
 }
 
-export default function SocialList({ title, socials, marginTop = 0, gap = 12, onClickHeader }: SocialListProps) {
+export default function SocialList({ title, tab, kind, marginTop = 0, gap = 12, onClickHeader }: SocialListProps) {
+  const { data } = useGetSocialList(kind, tab);
+
   return (
     <SocialListWrap marginTop={marginTop}>
       <SocialListHeader onClick={onClickHeader}>
         <Title>{title}</Title>
         <img src={more} alt="more" />
       </SocialListHeader>
-      <SocialCardList gap={gap} socials={socials} />
+      <SocialCardList gap={gap} socials={data} />
     </SocialListWrap>
   );
 }
@@ -37,6 +40,12 @@ export const SocialListHeader = styled.div`
   justify-content: space-between;
   margin-bottom: 8px;
   cursor: pointer;
+`;
+
+export const CardListWrap = styled.ul<{ gap: number }>`
+  display: flex;
+  flex-direction: column;
+  ${({ gap }) => `gap: ${gap}px`};
 `;
 
 export const Title = styled.span`
--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,8 +1,6 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import { useGetSocialList } from '../../api/social';
-import { useGetHotFeeds } from '../../api/feed';
 import ContentsArea from '../../common/components/ContentsArea';
 import Header from './Header';
 import Tabs from './Tabs';
@@ -14,10 +12,6 @@ const BASE_URL = '/home/list';
 
 export default function Home() {
   const [tab, setTab] = useState(0);
-  const { data: recentData } = useGetSocialList('recent', tab);
-  const { data: deadlineData } = useGetSocialList('deadline', tab);
-  const { data: popularData } = useGetSocialList('popular', tab);
-  const { data: feedsData } = useGetHotFeeds();
   const navigate = useNavigate();
 
   const handleClickTabs = useCallback((e: React.MouseEvent) => {
@@ -33,21 +27,24 @@ export default function Home() {
         <Tabs tab={tab} onClickTabs={handleClickTabs} />
         <SocialList
           title="인기 모임"
-          socials={popularData?.slice(0, 3)}
+          tab={tab}
+          kind={'popular'}
           marginTop={18}
           onClickHeader={() => navigate(`${BASE_URL}/popular`, { state: { tab } })}
         />
         <SocialList
           title="마감 임박 모임 🔥"
-          socials={deadlineData?.slice(0, 3)}
+          tab={tab}
+          kind={'deadline'}
           marginTop={100}
           onClickHeader={() => navigate(`${BASE_URL}/deadline`, { state: { tab } })}
         />
-        <FeedList title="주목받는 피드 ✨" feeds={feedsData} onClickHeader={() => navigate(`${BASE_URL}/feed`)} />
+        <FeedList title="주목받는 피드 ✨" onClickHeader={() => navigate(`${BASE_URL}/feed`)} />
         <SocialList
           title="최신 모임"
-          socials={recentData?.slice(0, 3)}
+          tab={tab}
           marginTop={54}
+          kind={'recent'}
           onClickHeader={() => navigate(`${BASE_URL}/recent`, { state: { tab } })}
         />
       </ContentsArea>
