41a940c16b6f21181798ff9bcf76105559698698
Chuhj <chj9574@naver.com>
2023-03-19 19:23:02 +0900

    feat: ✨ 무한 스크롤 적용

--- src/views/Feed/index.tsx
+++ src/views/Feed/index.tsx
@@ -1,5 +1,6 @@
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useInView } from 'react-intersection-observer';
 import styled from 'styled-components';
 import { useGetFeeds } from '../../api/feed';
 import BottomNav from '../../common/components/BottomNav';
@@ -8,21 +9,33 @@ import EachFeed from '../../components/HomeFeedList/EachFeed';
 import plus from '../../common/styles/assets/plus.svg';
 
 export default function Feed() {
-  const { data, refetch } = useGetFeeds();
-  const feeds = data?.data;
+  const { ref, inView } = useInView();
+  const { data, fetchNextPage } = useGetFeeds();
+
+  const feeds = useMemo(
+    () =>
+      data?.pages.reduce<any[]>((acc, data) => {
+        return [...acc, ...data.data];
+      }, []),
+    [data]
+  );
+
   const navigate = useNavigate();
 
   useEffect(() => {
-    refetch();
-  }, []);
+    if (inView && data?.pages[data?.pages.length - 1].hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView]);
 
   return (
     <div style={{ marginTop: '85px', marginBottom: '40px' }}>
       <Header tabName="피드" />
       <div style={{ width: 'fit-content', margin: '0 auto' }}>
         {feeds?.map((feed) => (
-          <EachFeed feed={feed} />
+          <EachFeed key={feed.postId} feed={feed} />
         ))}
+        <div ref={ref}></div>
       </div>
       <AddButton onClick={() => navigate('/feed/add')}>
         <img src={plus} alt="" />
